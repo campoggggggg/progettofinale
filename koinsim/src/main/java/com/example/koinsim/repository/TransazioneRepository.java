6abd483f80584cd16d1c9b6c9262d179f3727267
Miriam <153954154+bayloncina@users.noreply.github.com>
2026-04-17 11:21:47 +0200

    classi dto, model, repository

--- koinsim/src/main/java/com/example/koinsim/repository/TransazioneRepository.java
+++ koinsim/src/main/java/com/example/koinsim/repository/TransazioneRepository.java
@@ -1,5 +1,14 @@
 package com.example.koinsim.repository;
 
-public class TransazioneRepository {
-    
+import com.example.koinsim.model.Transazione;
+import com.example.koinsim.model.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransazioneRepository extends JpaRepository<Transazione, Long> {
+
+    List<Transazione> findByUtente(Utente utente);
+
+    List<Transazione> findByUtenteId(Long utenteId);
 }
--- koinsim/src/main/java/com/example/koinsim/repository/UtenteRepository.java
+++ koinsim/src/main/java/com/example/koinsim/repository/UtenteRepository.java
@@ -1,5 +1,15 @@
 package com.example.koinsim.repository;
 
-public class UtenteRepository {
-    
+import com.example.koinsim.model.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UtenteRepository extends JpaRepository<Utente, Long> {
+
+    Optional<Utente> findByNomeUtente(String nomeUtente);
+
+    boolean existsByNomeUtente(String nomeUtente);
+
+    boolean existsByEmail(String email);
 }
