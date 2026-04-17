179a95b9444c165242f263eabd9188113cfca2f8
Gabriele <gabriele.campomizzi@gmail.com>
2026-04-17 10:49:52 +0200

    cartelle create

---
+++ koinsim/src/main/java/com/example/koinsim/KoinsimApplication.java
@@ -0,0 +1,13 @@
+package com.example.koinsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoinsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoinsimApplication.class, args);
+	}
+
+}
