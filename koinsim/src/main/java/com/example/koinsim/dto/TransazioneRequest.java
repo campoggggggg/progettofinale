2eb15a1834fdaa0d968f55e848c13a5f6a74bba2
Miriam <153954154+bayloncina@users.noreply.github.com>
2026-04-20 10:37:31 +0200

    Fix data request

--- koinsim/src/main/java/com/example/koinsim/dto/TransazioneRequest.java
+++ koinsim/src/main/java/com/example/koinsim/dto/TransazioneRequest.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,6 +13,4 @@ public class TransazioneRequest {
     private String simbolo;
     private TipoAsset tipoAsset;
     private Double quantita;
-    //private Double prezzoDiAcquisto;
-    private LocalDate dataAcquisto;
 }
